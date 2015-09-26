5b333908b919bf5e2ef09ee958a9dae789a705c7
Pablo Novas <pjnovas@gmail.com>
2015-09-25 23:11:27 -0300

    added zoom and pan

--- src/floorplan/Viewport.js
+++ src/floorplan/Viewport.js
@@ -5,6 +5,7 @@
  */
 
 import Base from "./Base";
+import svgPanZoom from 'svg-pan-zoom';
 
 export default class Viewport extends Base {
 
@@ -20,9 +21,32 @@ export default class Viewport extends Base {
 
     this.container = SVG(this.options.el).size("100%", "100%");
 
-    //this.container.attr({
-    //  viewBox: "0 0 " + size.width + " " + size.height
-    //});
+    window.setTimeout(() => { // this is to svgPanZoom to work
+
+      this.panZoom = svgPanZoom(this.container.node, {
+          panEnabled: true
+        , controlIconsEnabled: true
+        , zoomEnabled: true
+        , dblClickZoomEnabled: true
+        , mouseWheelZoomEnabled: true
+        , preventMouseEventsDefault: true
+        , zoomScaleSensitivity: 0.2
+        , minZoom: 0.5
+        , maxZoom: 10
+        , fit: true
+        , contain: false
+        , center: true
+        , refreshRate: 'auto'
+        //, beforeZoom: function(){}
+        //, onZoom: function(){}
+        //, beforePan: function(){}
+        //, onPan: function(){}
+        //, customEventsHandler: {}
+      });
+
+      // expose to global for testing
+      window.panZoom = this.panZoom;
+    }, 1);
 
     this.container.attr(this.options.attrs);
     this.container.addClass("viewport");
